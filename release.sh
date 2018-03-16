84dcb7b775ec17d71285e141821080fdd184d947
Ryan Dawson <ryan.dawson@alfresco.com>
2018-03-16 19:15:01 +0000

    configure authenticated git access

--- release.sh
+++ release.sh
@@ -5,6 +5,10 @@ echo "BAMBOO_OPTS=${BAMBOO_OPTS}"
 
 SCRIPT_DIR="$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)"
 echo $SCRIPT_DIR
+GIT_PROJECT=basename $(pwd)
+echo $GIT_PROJECT
+
+git remote set-url origin https://${GIT_AUTHOR_NAME}:${GIT_PASSWORD}@github.com/Activiti/$GIT_PROJECT.git
 
 git checkout develop
 
