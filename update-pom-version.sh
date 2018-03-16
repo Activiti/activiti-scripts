124af35fefec740b6d476ea06b534a3fa89b786e
Ryan Dawson <ryandawson@cantab.net>
2018-03-16 17:15:03 +0000

    set versions specifically in case we use a version that a dep library also uses (#1)

--- update-pom-version.sh
+++ update-pom-version.sh
@@ -4,8 +4,24 @@ echo update pom version from ${VERSION} to ${NEXT_VERSION}
 
 if [[ "$OSTYPE" == "darwin"* ]]
 then
-  find . -name pom.xml -exec sed -i.bak -e "s/${VERSION}/${NEXT_VERSION}/g" {} \;
+  find . -name pom.xml -exec sed -i.bak -e "s/<version>${VERSION}<\/version>/<version>${NEXT_VERSION}<\/version>/g" {} \;
+  find . -name pom.xml -exec sed -i.bak -e "s/<activiti.version>${VERSION}/<\/activiti.version><activiti.version>${NEXT_VERSION}<\/activiti.version>/g" {} \;
+  find . -name pom.xml -exec sed -i.bak -e "s/<activiti-cloud-service-common.version>${VERSION}<\/activiti-cloud-service-common.version>/<activiti-cloud-service-common.version>${NEXT_VERSION}<\/activiti-cloud-service-common.version>/g" {} \;
+  find . -name pom.xml -exec sed -i.bak -e "s/<activiti-cloud-runtime-bundle.version>${VERSION}<\/activiti-cloud-runtime-bundle.version>/<activiti-cloud-runtime-bundle.version>${NEXT_VERSION}<\/activiti-cloud-runtime-bundle.version>/g" {} \;
+  find . -name pom.xml -exec sed -i.bak -e "s/<activiti-cloud-audit.version>${VERSION}<\/activiti-cloud-audit.version>/<activiti-cloud-audit.version>${NEXT_VERSION}<\/activiti-cloud-audit.version>/g" {} \;
+  find . -name pom.xml -exec sed -i.bak -e "s/<activiti-cloud-query.version>${VERSION}<\/activiti-cloud-query.version>/<activiti-cloud-query.version>${NEXT_VERSION}<\/activiti-cloud-query.version>/g" {} \;
+  find . -name pom.xml -exec sed -i.bak -e "s/<activiti-cloud-connectors.version>${VERSION}<\/activiti-cloud-connectors.version>/<activiti-cloud-connectors.version>${NEXT_VERSION}<\/activiti-cloud-connectors.version>/g" {} \;
+  find . -name pom.xml -exec sed -i.bak -e "s/<activiti-cloud-process-model.version>${VERSION}<\/activiti-cloud-process-model.version>/<activiti-cloud-process-model.version>${NEXT_VERSION}<\/activiti-cloud-process-model.version>/g" {} \;
+  find . -name pom.xml -exec sed -i.bak -e "s/<activiti-cloud-org.version>${VERSION}<\/activiti-cloud-org.version>/<activiti-cloud-org.version>${NEXT_VERSION}<\/activiti-cloud-org.version>/g" {} \;
   find . -name pom.xml.bak -delete
 else
-  find . -name pom.xml -exec sed -i -e "s/${VERSION}/${NEXT_VERSION}/g" {} \;
+  find . -name pom.xml -exec sed -i -e "s/<version>${VERSION}<\/version>/<version>${NEXT_VERSION}<\/version>/g" {} \;
+  find . -name pom.xml -exec sed -i -e "s/<activiti.version>${VERSION}/<\/activiti.version><activiti.version>${NEXT_VERSION}<\/activiti.version>/g" {} \;
+  find . -name pom.xml -exec sed -i -e "s/<activiti-cloud-service-common.version>${VERSION}<\/activiti-cloud-service-common.version>/<activiti-cloud-service-common.version>${NEXT_VERSION}<\/activiti-cloud-service-common.version>/g" {} \;
+  find . -name pom.xml -exec sed -i -e "s/<activiti-cloud-runtime-bundle.version>${VERSION}<\/activiti-cloud-runtime-bundle.version>/<activiti-cloud-runtime-bundle.version>${NEXT_VERSION}<\/activiti-cloud-runtime-bundle.version>/g" {} \;
+  find . -name pom.xml -exec sed -i -e "s/<activiti-cloud-audit.version>${VERSION}<\/activiti-cloud-audit.version>/<activiti-cloud-audit.version>${NEXT_VERSION}<\/activiti-cloud-audit.version>/g" {} \;
+  find . -name pom.xml -exec sed -i -e "s/<activiti-cloud-query.version>${VERSION}<\/activiti-cloud-query.version>/<activiti-cloud-query.version>${NEXT_VERSION}<\/activiti-cloud-query.version>/g" {} \;
+  find . -name pom.xml -exec sed -i -e "s/<activiti-cloud-connectors.version>${VERSION}<\/activiti-cloud-connectors.version>/<activiti-cloud-connectors.version>${NEXT_VERSION}<\/activiti-cloud-connectors.version>/g" {} \;
+  find . -name pom.xml -exec sed -i -e "s/<activiti-cloud-process-model.version>${VERSION}<\/activiti-cloud-process-model.version>/<activiti-cloud-process-model.version>${NEXT_VERSION}<\/activiti-cloud-process-model.version>/g" {} \;
+  find . -name pom.xml -exec sed -i -e "s/<activiti-cloud-org.version>${VERSION}<\/activiti-cloud-org.version>/<activiti-cloud-org.version>${NEXT_VERSION}<\/activiti-cloud-org.version>/g" {} \;
 fi
