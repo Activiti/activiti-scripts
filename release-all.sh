bcb74cec3af9e82a619abd7cc52014c708954625
Ryan Dawson <ryan.dawson@alfresco.com>
2018-03-16 13:43:11 +0000

    dry run doesn't exist

--- release-all.sh
+++ release-all.sh
@@ -3,4 +3,4 @@ set -e
 
 SCRIPT_DIR="$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)"
 
-SCRIPT="${SCRIPT_DIR}/release-dry-run.sh" ${SCRIPT_DIR}/run.sh
+SCRIPT="${SCRIPT_DIR}/release.sh" ${SCRIPT_DIR}/run.sh
