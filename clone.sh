616f8c9a4c8397266e35c9cf6eace33efc8bf4e8
Ryan Dawson <ryan.dawson@alfresco.com>
2018-03-19 10:30:20 +0000

    option to clone particular branch

--- clone.sh
+++ clone.sh
@@ -3,4 +3,14 @@ set -e
 
 [ -n "${PULL}" ] && git pull --rebase
 
-echo "cloned into $(pwd)"
+git fetch
+if [ -z "${BRANCH}" ];
+ then
+ echo "Using default branch";
+else
+ git fetch
+ echo "Checking out branch '${BRANCH}' for $(pwd)";
+ git checkout ${BRANCH};
+fi
+
+echo "cloned into $(REPO_DIR)"
