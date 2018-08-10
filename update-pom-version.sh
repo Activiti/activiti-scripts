e0da33f08050330e05a4a52e47639cc6af86371d
Ryan Dawson <ryan.dawson@alfresco.com>
2018-08-10 16:53:17 +0100

    do global sed to be sure to match version

--- update-pom-version.sh
+++ update-pom-version.sh
@@ -2,13 +2,7 @@
 
 echo update pom version from ${VERSION} to ${NEXT_VERSION}
 
-for DEP in '' activiti activiti-cloud-service-common activiti-cloud-runtime-bundle activiti-cloud-audit activiti-cloud-query activiti-cloud-connectors activiti-cloud-app activiti-cloud-process-model activiti-cloud-org activiti\.cloud
-do
-  [ -n "$DEP" ] && TAG="${DEP}."
-  TAG="${TAG}version"
-  [ -n "${SED_REPLACEMENTS}" ] && SED_REPLACEMENTS="${SED_REPLACEMENTS} "
-  SED_REPLACEMENTS="${SED_REPLACEMENTS}-e 's@<${TAG}>${VERSION}</${TAG}>@<${TAG}>${NEXT_VERSION}</${TAG}>@g'"
-done
+SED_REPLACEMENTS="${SED_REPLACEMENTS}-e 's@>${VERSION}<@>${NEXT_VERSION}<@g'"
 
 if [[ "$OSTYPE" == "darwin"* ]]
 then
