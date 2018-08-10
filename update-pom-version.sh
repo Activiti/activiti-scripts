89673f271b31a5c89b3123c939076effe25c13eb
Ryan Dawson <ryan.dawson@alfresco.com>
2018-08-10 17:22:06 +0100

    try just example cloud connector and gateway

--- update-pom-version.sh
+++ update-pom-version.sh
@@ -2,7 +2,13 @@
 
 echo update pom version from ${VERSION} to ${NEXT_VERSION}
 
-SED_REPLACEMENTS="${SED_REPLACEMENTS}-e 's@>${VERSION}<@>${NEXT_VERSION}<@g'"
+for DEP in '' activiti activiti-cloud-service-common activiti-cloud-runtime-bundle activiti-cloud-audit activiti-cloud-query activiti-cloud-connectors activiti-cloud-app activiti-cloud-process-model activiti-cloud-org activiti\.cloud
+do
+  [ -n "$DEP" ] && TAG="${DEP}."
+  TAG="${TAG}version"
+  [ -n "${SED_REPLACEMENTS}" ] && SED_REPLACEMENTS="${SED_REPLACEMENTS} "
+  SED_REPLACEMENTS="${SED_REPLACEMENTS}-e 's@<${TAG}>${VERSION}</${TAG}>@<${TAG}>${NEXT_VERSION}</${TAG}>@g'"
+done
 
 if [[ "$OSTYPE" == "darwin"* ]]
 then
