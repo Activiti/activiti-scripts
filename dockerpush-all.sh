2562437bc6251b07e84f99a133898818221b3dd1
Ryan Dawson <ryan.dawson@alfresco.com>
2018-08-13 15:49:41 +0100

    get more info on current dir and what is in it

--- dockerpush-all.sh
+++ dockerpush-all.sh
@@ -3,4 +3,15 @@ set -ex
 
 SCRIPT_DIR="$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)"
 
+echo "SCRIPT_DIR contains"
+
+for entry in "$SCRIPT_DIR"/*
+do
+  echo "$entry"
+done
+
+echo "ls -ltr returns"
+
+ls -ltr
+
 SCRIPT="${SCRIPT_DIR}/dockerpush.sh" ${SCRIPT_DIR}/run.sh
