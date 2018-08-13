65e2c6d4aef4854b36be9eee0b87df4e48bf47b8
Ryan Dawson <ryan.dawson@alfresco.com>
2018-08-13 16:12:18 +0100

    dont need as much debugging now

--- dockerpush-all.sh
+++ dockerpush-all.sh
@@ -3,15 +3,4 @@ set -ex
 
 SCRIPT_DIR="$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)"
 
-echo "SCRIPT_DIR contains"
-
-for entry in "$SCRIPT_DIR"/*
-do
-  echo "$entry"
-done
-
-echo "ls -ltr returns"
-
-ls -ltr
-
 SCRIPT="${SCRIPT_DIR}/dockerpush.sh" ${SCRIPT_DIR}/run.sh
