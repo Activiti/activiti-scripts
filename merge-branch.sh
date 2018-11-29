6bb6d2eaa1077b46ac1780e7483bc5a809d2b1ea
ryandawsonuk <ryandawson@cantab.net>
2018-11-29 14:54:38 +0000

    example projects now have master but not develop

--- merge-branch.sh
+++ merge-branch.sh
@@ -3,18 +3,28 @@ set -e
 
 [ -z "${BRANCH}" ] && exit
 
-git checkout develop
+BASEBRANCH=develop
+
+DEVEXISTS=$(git show-ref refs/heads/develop)
+
+if [ -n "$DEVEXISTS" ];
+  then
+    echo 'using develop as base branch'
+  else
+    BASEBRANCH=master
+fi
+git checkout ${BASEBRANCH}
 git pull --rebase
 git checkout ${BRANCH} || break
 git pull --rebase
-git rebase develop
-git checkout develop
+git rebase ${BASEBRANCH}
+git checkout ${BASEBRANCH}
 git merge --no-ff --no-edit ${BRANCH}
 
 if [ -n "${GIT_PUSH}" ]
 then
   echo "* pushing to origin"
-  git push --force-with-lease --atomic origin ${BRANCH} develop
+  git push --force-with-lease --atomic origin ${BRANCH} ${BASEBRANCH}
   git push origin :${BRANCH}
   git branch -d ${BRANCH}
 fi
