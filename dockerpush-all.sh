5d310d8559fdca3373e243acfb28fe5ecb913c2b
salaboy <Salaboy@gmail.com>
2019-05-07 16:15:26 +0100

    Update dockerpush-all.sh

--- dockerpush-all.sh
+++ dockerpush-all.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -ex
 
-export SCRIPT_DIR="$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)"
+export SCRIPT_DIR="$(cd "$( dirname "$0" )" && pwd)"
 
 SCRIPT="${SCRIPT_DIR}/dockerpush.sh" . ${SCRIPT_DIR}/run.sh
