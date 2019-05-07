53550f8c44ba867eb8eaa1f4b7a10bbaf0c3b219
salaboy <Salaboy@gmail.com>
2019-05-07 17:21:26 +0100

    Update build-all.sh

--- build-all.sh
+++ build-all.sh
@@ -4,4 +4,4 @@ set -e
 export SCRIPT_DIR="$(cd "$( dirname "$0" )" && pwd)"
 echo "SCRIPT_DIR IS $SCRIPT_DIR"
 
-SCRIPT="${SCRIPT_DIR}/build.sh" . ${SCRIPT_DIR}/run.sh
+SCRIPT="${SCRIPT_DIR}/build.sh" ${SCRIPT_DIR}/run.sh
