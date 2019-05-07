a7adc01de4b7a7aa30d7c2a01174c3aff88f6aa1
salaboy <Salaboy@gmail.com>
2019-05-07 16:07:56 +0100

    Update clone-all.sh

--- clone-all.sh
+++ clone-all.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -e
 
-export SCRIPT_DIR="$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)"
+export SCRIPT_DIR="$(cd "$( dirname "$0" )" && pwd)"
 
 SCRIPT="${SCRIPT_DIR}/clone.sh" ${SCRIPT_DIR}/run.sh
