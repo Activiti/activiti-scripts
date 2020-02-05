aa775d21bfc1268bd88d83bd2a2f77b41ba3f81f
Elias Ricken de Medeiros <26007058+erdemedeiros@users.noreply.github.com>
2020-02-05 16:11:31 +0100

    Revert "Put back https URL"
    This reverts commit 0494b202ea4de430c28ce4adc7946898bbdcae93.

--- release.sh
+++ release.sh
@@ -7,7 +7,6 @@ GIT_PROJECT=$(basename $(pwd))
 echo "RELEASING PROJECT $GIT_PROJECT from $(pwd)"
 echo "SCRIPT_DIR IS $SCRIPT_DIR"
 
-git remote set-url origin https://${GIT_AUTHOR_NAME}:${GIT_PASSWORD}@github.com/Activiti/${GIT_PROJECT}.git
 git fetch --tags
 if  git tag --list | egrep -q "^$RELEASE_VERSION$"
 then
