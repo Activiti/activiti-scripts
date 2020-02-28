bb94dee9ff56d146c4e9ce89d775ba3a95f26915
Elias Ricken de Medeiros <26007058+erdemedeiros@users.noreply.github.com>
2020-02-28 18:12:51 +0100

    Force tag override

--- create-tag.sh
+++ create-tag.sh
@@ -6,5 +6,5 @@ echo "Version to release ${VERSION_TO_RELEASE}"
 git remote rm origin
 git remote add origin https://"${GITHUB_TOKEN}":x-oauth-basic@github.com/Activiti/activiti-scripts.git
 
-git tag -a "${VERSION_TO_RELEASE}" -m "Release version ${VERSION_TO_RELEASE}"
+git tag -fa "${VERSION_TO_RELEASE}" -m "Release version ${VERSION_TO_RELEASE}"
 git push origin "${VERSION_TO_RELEASE}"
