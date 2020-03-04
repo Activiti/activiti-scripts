c8c9fade6744dc44bee783c7e2a86d944ab577a6
Elias Ricken de Medeiros <26007058+erdemedeiros@users.noreply.github.com>
2020-03-04 11:29:15 +0100

    Add logs

--- fetch-versions.sh
+++ fetch-versions.sh
@@ -83,6 +83,8 @@ for i in "${projects[@]}"; do
 
   if [ ! -z "$2" ]; then
     # adding 'v' to tag to align it with the format of internal versions: 'v7.1.68'
+    echo "currenty directory $(pwd)"
+    echo "Available tags: $(git tag --list)"
     for k in $(git tag --list 'v*' | cut -d'v' -f 2); do
       if [ "$k" = "$2" ]; then
         exist=1
