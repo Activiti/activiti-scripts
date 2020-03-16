b508232364a022fa4bc2564fe6cf9b23d9a3cc0c
Elias Ricken de Medeiros <26007058+erdemedeiros@users.noreply.github.com>
2020-03-16 16:33:36 +0100

    Ask confirmation before deleting tags

--- delete-all-tags.sh
+++ delete-all-tags.sh
@@ -4,9 +4,13 @@ set -e
 RELEASE_VERSION=$(<VERSION)
 export RELEASE_VERSION
 
-git push origin :"${RELEASE_VERSION}" || true
+read -p "Delete tag ${RELEASE_VERSION} from remote? " -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]]; then
+  git push origin :"${RELEASE_VERSION}" || true
 
-SCRIPT_DIR="$(cd "$( dirname "$0" )" && pwd)"
-export SCRIPT_DIR
+  SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
+  export SCRIPT_DIR
 
-SCRIPT="${SCRIPT_DIR}/delete-tag.sh" "${SCRIPT_DIR}"/run.sh
+  SCRIPT="${SCRIPT_DIR}/delete-tag.sh" "${SCRIPT_DIR}"/run.sh
+fi
