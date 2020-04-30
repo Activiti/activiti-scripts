afe3aba30fe8a51438ef57e815a930d9bc7ce43c
Alex Merkulov <almerico@gmail.com>
2020-04-30 19:08:19 +0300

    helm function

--- release-helm.sh
+++ release-helm.sh
@@ -1,12 +1,11 @@
 #!/usr/bin/env bash
 set -ex
 
-function git_clone() {
   git clone https://${GITHUB_TOKEN}:x-oauth-basic@github.com/Activiti/activiti-cloud-application.git
   cp VERSION  activiti-cloud-application/activiti-cloud-dependencies/
-}
 
-function prepare_and_run_helm() {
+
+
   cd activiti-cloud-application/activiti-cloud-dependencies
   mvn -q versions:set -Droot.log.level=off -DnewVersion=${VERSION}
   make updatebot/push-version-dry
@@ -15,19 +14,19 @@ function prepare_and_run_helm() {
   make run-helm-chart
   sleep 120
   cd -
-}
 
-function run_acceptance_test() {
+
+
   cd activiti-cloud-application/activiti-cloud-acceptance-scenarios
   mvn -DskipITs -DskipTests -q clean install -f activiti-cloud-acceptance-scenarios/pom.xml
   mvn -pl 'modeling-acceptance-tests' -Droot.log.level=off -q clean verify
   mvn -pl 'runtime-acceptance-tests'  -Droot.log.level=off -q clean verify
   cd -
-}
 
-function upload_helm_chart() {
+
+
   cd activiti-cloud-application/activiti-cloud-dependencies
   make tag
   make github
   cd -
-}
+
