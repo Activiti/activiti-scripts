50a172373dcbaa529073c7759532f2e0e71a5f8f
Alex Merkulov <almerico@gmail.com>
2020-04-30 18:20:58 +0300

    functions added

--- release-helm-chart.sh
+++ release-helm-chart.sh
@@ -1,28 +1,33 @@
 #!/usr/bin/env bash
 set -e
-git clone https://${GITHUB_TOKEN}:x-oauth-basic@github.com/Activiti/activiti-cloud-application.git
 
-cd activiti-cloud-application;
-mvn -DskipITs -DskipTests -q clean install -f activiti-cloud-acceptance-scenarios/pom.xml
-pwd
-ls
-cp ../VERSION  activiti-cloud-dependencies/
-echo "____________________________________"
-ls ..
-cd -
-cd activiti-cloud-dependencies
-mvn -q versions:set -Droot.log.level=off -DnewVersion=${VERSION}
-make updatebot/push-version-dry
-sleep 20
-make prepare-helm-chart
-make run-helm-chart
-cd -
-sleep 120
-cd activiti-cloud-acceptance-scenarios
-mvn -pl 'modeling-acceptance-tests' -Droot.log.level=off -q clean verify
-mvn -pl 'runtime-acceptance-tests'  -Droot.log.level=off -q clean verify
-cd -
-cd activiti-cloud-dependencies
-make tag
-make github
-cd -
+function git_clone() {
+  git clone https://${GITHUB_TOKEN}:x-oauth-basic@github.com/Activiti/activiti-cloud-application.git
+  cp VERSION  activiti-cloud-application/activiti-cloud-dependencies/
+}
+
+function prepare_and_run_helm() {
+  cd activiti-cloud-application/activiti-cloud-dependencies
+  mvn -q versions:set -Droot.log.level=off -DnewVersion=${VERSION}
+  make updatebot/push-version-dry
+  sleep 20
+  make prepare-helm-chart
+  make run-helm-chart
+  sleep 120
+  cd -
+}
+
+function run_acceptance_test() {
+  cd activiti-cloud-application/activiti-cloud-acceptance-scenarios
+  mvn -DskipITs -DskipTests -q clean install -f activiti-cloud-acceptance-scenarios/pom.xml
+  mvn -pl 'modeling-acceptance-tests' -Droot.log.level=off -q clean verify
+  mvn -pl 'runtime-acceptance-tests'  -Droot.log.level=off -q clean verify
+  cd -
+}
+
+function upload_helm_chart() {
+  cd activiti-cloud-application/activiti-cloud-dependencies
+  make tag
+  make github
+  cd -
+}
