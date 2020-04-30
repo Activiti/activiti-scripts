6429c6ab85002605e18dc08d37ca6fd00ded95e8
Elias Ricken de Medeiros <26007058+erdemedeiros@users.noreply.github.com>
2020-04-30 12:03:09 +0200

    Fix release of examples images
    - stop using release-all only to switch of branch

--- release-docker-activiti-cloud-example-images.sh
+++ release-docker-activiti-cloud-example-images.sh
@@ -9,17 +9,19 @@ export PROJECTS=activiti-cloud-application
 RELEASE_VERSION=$(<VERSION)
 export RELEASE_VERSION
 
-./release-all.sh #switch to the right tag
 SRC_DIR=${SRC_DIR:-$HOME/src}
 echo "SCRIPT_DIR ${SRC_DIR}"
 cd ${SRC_DIR}/${PROJECTS} || exit 1
+
+git fetch --tags
+git checkout "${RELEASE_VERSION}"
+
 export DOCKER_IMAGES="example-runtime-bundle,activiti-cloud-query,example-cloud-connector,activiti-cloud-modeling"
 
 for DOCKER_IMAGE in ${DOCKER_IMAGES//,/ }
 do
   cd ${DOCKER_IMAGE} || exit 1
   ${SCRIPT_DIR}/dockerpush.sh
-echo $(basename $(pwd))
   cd .. || exit 1
 done
 
