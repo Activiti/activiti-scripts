e8d6dd0d76cf32ec1b559525d7c30de209d7a53a
Alex Merkulov <almerico@gmail.com>
2020-04-30 14:53:45 +0300

    kubernetes stuff

---
+++ release-helm-chart.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+set -e
+export activiti-cloud-application
+git clone https://"${GITHUB_TOKEN}":x-oauth-basic@github.com/Activiti/activiti-cloud-application.git
+
+cd activiti-cloud-application;
+mvn -DskipITs -DskipTests -q -f  activiti-cloud-acceptance-scenarios/pom.xml
+cd -
+cp VERSION  activiti-cloud-dependencies/
+cd activiti-cloud-dependencies
+mvn -q versions:set -Droot.log.level=off -DnewVersion=$(VERSION)
+make updatebot/push-version-dry
+sleep 20
+make prepare-helm-chart
+make run-helm-chart
+cd -
+sleep 120
+cd activiti-cloud-acceptance-scenarios
+mvn -pl 'modeling-acceptance-tests' -Droot.log.level=off -q clean verify
+mvn -pl 'runtime-acceptance-tests'  -Droot.log.level=off -q clean verify
+cd -
