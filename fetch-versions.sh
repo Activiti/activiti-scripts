797bb7f79bc8aff195c44e6bd30937a46dca12cd
Elias Ricken de Medeiros <26007058+erdemedeiros@users.noreply.github.com>
2020-05-01 20:42:06 +0200

    Update release based on activiti-cloud-application (#31)

--- fetch-versions.sh
+++ fetch-versions.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 
 writeVersionOnFile() {
-  local pom_path=$1
-  local destination_file=$2
-  local current_dependency=$3
-  local related_repository=$4
+  local destination_file=$1
+  local version=$2
+  local related_repository=$3
+
+  echo "${related_repository}: ${version}"
   echo -n "$related_repository " >>"$destination_file"
-  version=$(grep -v "Downloading" <"$pom_path" | grep "$current_dependency.version" | grep -om1 "7.[0-9]*.[0-9]*")
   echo "$version" >>"$destination_file"
 
   # check for the existence of such version for current project
@@ -17,23 +17,18 @@ writeVersionOnFile() {
   fi
 }
 
-parseVersions() {
-  local pom_path=$1
-  local property_pattern=$2
-  local destination_file=$3
-  local name_dependency_aggregator=$4
+parseActivitiCloudVersion() {
+  local pom_path="activiti-cloud-dependencies/pom.xml"
+  local property_pattern="activiti-cloud\.version"
 
-  if [ "$name_dependency_aggregator" == "activiti-cloud-application" ]; then
-    for current_dependency in $(cat $pom_path | grep -v "Downloading" | grep $property_pattern | grep "version" | grep "7." | cut -d'<' -f 2 | cut -d'.' -f 1); do
-      if [ "$current_dependency" == "activiti-cloud-build" ]; then
-        writeVersionOnFile "$pom_path" "$destination_file" "$current_dependency" "activiti-cloud"
-      fi
-    done
-  else
-    for current_dependency in $(cat $pom_path | grep -v "Downloading" | grep $property_pattern | grep "version" | grep "7." | cut -d'<' -f 2 | cut -d'.' -f 1); do
-      writeVersionOnFile "$pom_path" "$destination_file" "$current_dependency" "$current_dependency"
-    done
-  fi
+  local activiti_cloud_version=$(grep "activiti-cloud\.version" <"$pom_path" -m1 | grep -om1 "${activiti_version_pattern}")
+  writeVersionOnFile "$activiti_cloud_file" "$activiti_cloud_version" "activiti-cloud"
+
+  local cloud_api_url="https://raw.githubusercontent.com/Activiti/activiti-cloud/v${activiti_cloud_version}/activiti-cloud-api/pom.xml"
+  echo "Getting Activiti core version from: ${cloud_api_url} "
+  local activiti_core_version=$(curl https://raw.githubusercontent.com/Activiti/activiti-cloud/v${activiti_cloud_version}/activiti-cloud-api/pom.xml \
+    | grep "activiti\.version" -m1 | grep -o "${activiti_version_pattern}")
+  writeVersionOnFile "${activiti_core_file}" "${activiti_core_version}" "Activiti"
 }
 
 updateRepoFile() {
@@ -46,148 +41,101 @@ updateRepoFile() {
 }
 
 original_directory=$(pwd)
+name_dependency_aggregator=activiti-cloud-application
+activiti_version_pattern="7\.[[:digit:]]\{1,3\}\.[[:digit:]]\{1,6\}"
+
+mkdir /tmp/release-versions && cd /tmp/release-versions || exit 1
+git clone -q https://github.com/Activiti/$name_dependency_aggregator.git && cd $name_dependency_aggregator || exit 1
 
 activiti_core_file=repos-activiti.txt
+activiti_cloud_file=repos-activiti-cloud.txt
+activiti_cloud_application_file=repos-activiti-cloud-application.txt
+modeling_app_file=repos-activiti-modeling-app.txt
 
-if [ -n "$1" ]; then
-  if [ "$1" = "Activiti" ] || [ "$1" = "activiti-cloud" ] || [ "$1" = "activiti-cloud-application" ]; then
-    projects=$1
+echo "Handling $name_dependency_aggregator"
+git fetch --tags
+
+tag_to_fetch="$1"
+
+if [ -n "${tag_to_fetch}" ]; then
+  # adding 'v' to tag to align it with the format of internal versions: 'v7.1.68'
+  for k in $(git tag --list 'v*' | cut -d'v' -f 2); do
+    if [ "$k" = "${tag_to_fetch}" ]; then
+      exist=1
+      break
+    else
+      exist=0
+    fi
+  done
+
+  if [ "$exist" -eq 1 ]; then
+    git checkout -q tags/v"${tag_to_fetch}"
+    version_dependency_aggregator=${tag_to_fetch}
   else
-    echo "Incorrect project name '$1'"
-    echo "Choose among: Activiti or activiti-cloud or activiti-cloud-application"
-    echo "Leave blank to update all projects"
+    echo "The provided version '${tag_to_fetch}' does not exist"
+    cd ../..
+    rm -rf release-versions
+    exit 1
   fi
+
 else
-  projects=(Activiti activiti-cloud activiti-cloud-application)
+  # if no second argument is provided, we get the latest tag
+  latest_tag=$(git tag --sort=-creatordate | head -n 1)
+
+  if [[ ${latest_tag::1} == "v" ]]; then
+    aggregator_tag=$(git tag --sort=-creatordate | head -n 1)
+  else
+    aggregator_tag=$(git tag --sort=-creatordate | head -n 2 | grep "v")
+  fi
+
+  git checkout -q tags/"$aggregator_tag"
+  version_dependency_aggregator=$(echo "$aggregator_tag" | cut -d'v' -f 2)
 fi
 
-for project in "${projects[@]}"; do
-  mkdir /tmp/release-versions && cd /tmp/release-versions
-  git clone -q https://github.com/Activiti/$project.git && cd $project
+parseActivitiCloudVersion $activiti_cloud_file
 
-  case "$project" in
-  'Activiti')
-    file=${activiti_core_file}
-    ;;
-  'activiti-cloud')
-    file=repos-activiti-cloud.txt
-    ;;
-  'activiti-cloud-application')
-    file=repos-activiti-cloud-application.txt
-    examples_file=repos-activiti-cloud-examples.txt
-    bom_file=repos-activiti-cloud-bom.txt
-    modeling_app_file=repos-activiti-cloud-modeling-app.txt
-    ;;
-  esac
-
-  name_dependency_aggregator=$project
-  echo "Handling $name_dependency_aggregator"
-  git fetch --tags
-
-  if [ -n "$2" ]; then
-    # adding 'v' to tag to align it with the format of internal versions: 'v7.1.68'
-    for k in $(git tag --list 'v*' | cut -d'v' -f 2); do
-      if [ "$k" = "$2" ]; then
-        exist=1
-        break
-      else
-        exist=0
-      fi
-    done
-
-    if [ "$exist" -eq 1 ]; then
-      git checkout -q tags/v$2
-      version_dependency_aggregator=$2
-    else
-      echo "The provided version does not exist"
-      cd ../..
-      rm -rf release-versions
-      exit 1
-    fi
-
+if [ -n "$SHOULD_INCREMENT_VERSION" ]; then
+  ls
+  BETA_SUFFIX_PATTERN="\-beta[[:digit:]]\{1,3\}"
+  VERSION_PREFIX=$(curl https://raw.githubusercontent.com/Activiti/activiti-cloud-application/develop/pom.xml |
+    grep "<version>" | grep "\-SNAPSHOT" | grep -o -m1 "[[:digit:]]\{1,2\}\.[[:digit:]]\{1,3\}\.[[:digit:]]\{1,3\}")
+  LATEST_BETA_VERSION=$(git tag --sort=-creatordate | grep -m1 "${VERSION_PATTERN}${BETA_SUFFIX_PATTERN}")
+  if [ -z "$LATEST_BETA_VERSION" ]; then
+    NEXT_BETA_VERSION="${VERSION_PREFIX}-beta1"
   else
-    # if no second argument is provided, we get the latest tag
-    latest_tag=$(git tag --sort=-creatordate | head -n 1)
-
-    if [[ ${latest_tag::1} == "v" ]]; then
-      aggregator_tag=$(git tag --sort=-creatordate | head -n 1)
-    else
-      aggregator_tag=$(git tag --sort=-creatordate | head -n 2 | grep "v")
-    fi
-
-    git checkout -q tags/$aggregator_tag
-    version_dependency_aggregator=$(echo $aggregator_tag | cut -d'v' -f 2)
+    echo "Latest beta version: ${LATEST_BETA_VERSION}"
+    CURRENT_COUNT=$(echo "${LATEST_BETA_VERSION}" | grep -o "${BETA_SUFFIX_PATTERN}" | grep -o "[[:digit:]]\{1,3\}")
+    echo "Current count: $CURRENT_COUNT"
+    NEXT_BETA_VERSION="${VERSION_PREFIX}-beta$((CURRENT_COUNT + 1))"
   fi
+  echo "Next version: $NEXT_BETA_VERSION"
+  echo "${NEXT_BETA_VERSION}" >VERSION
+fi
 
-  # name and version of the projects in this aggregator
-  parseVersions pom.xml "activiti" $file "$name_dependency_aggregator"
-  if [ -n "$examples_file" ]; then
-    parseVersions activiti-cloud-dependencies/dependencies-tests/pom.xml "activiti\|example-" $examples_file
-  fi
+# addition of modeling front end project
+modeling_app_tags=$(curl -s https://api.github.com/repos/Activiti/activiti-modeling-app/tags | grep name | head -n3)
+echo "Latest tags for modeling app:"
+echo "$modeling_app_tags"
+modeling_app_version=$(echo "$modeling_app_tags" | cut -d'v' -f 2 | cut -d'"' -f 1 | head -n1)
+if [ -z "$modeling_app_version" ]; then
+  echo "Error: Unable to detect latest tag for modeling app."
+  exit 1
+fi
 
-  if [ -n "$SHOULD_INCREMENT_VERSION" ]; then
-    ls
-    BETA_SUFFIX_PATTERN="\-beta[[:digit:]]\{1,3\}"
-    VERSION_PREFIX=$(curl https://raw.githubusercontent.com/Activiti/activiti-cloud-application/develop/pom.xml \
-    | grep "<version>" | grep "\-SNAPSHOT" | grep -o -m1 "[[:digit:]]\{1,2\}\.[[:digit:]]\{1,3\}\.[[:digit:]]\{1,3\}")
-    LATEST_BETA_VERSION=$(git tag --sort=-creatordate | grep -m1 "${VERSION_PATTERN}${BETA_SUFFIX_PATTERN}")
-    if [ -z "$LATEST_BETA_VERSION" ]; then
-      NEXT_BETA_VERSION="${VERSION_PREFIX}-beta1"
-    else
-      echo "Latest beta version: ${LATEST_BETA_VERSION}"
-      CURRENT_COUNT=$(echo "${LATEST_BETA_VERSION}" | grep -o "${BETA_SUFFIX_PATTERN}" | grep -o "[[:digit:]]\{1,3\}")
-      echo "Current count: $CURRENT_COUNT"
-      NEXT_BETA_VERSION="${VERSION_PREFIX}-beta$((CURRENT_COUNT + 1))"
-    fi
-    echo "Next version: $NEXT_BETA_VERSION"
-    echo "${NEXT_BETA_VERSION}" >VERSION
-  fi
+writeVersionOnFile "$modeling_app_file" "$modeling_app_version" "activiti-modeling-app"
 
-  if [ "$name_dependency_aggregator" == "activiti-cloud-application" ]; then
-    # addition of modeling front end project
-    modeling_app_tags=$(curl -s https://api.github.com/repos/Activiti/activiti-modeling-app/tags | grep name | head -n10)
-    echo "Latest tags for modeling app:"
-    echo "$modeling_app_tags"
-    modeling_app_version=$(echo "$modeling_app_tags" | cut -d'v' -f 2 | cut -d'"' -f 1 | head -n1)
-    if [ -z "$modeling_app_version" ]; then
-      echo "Error: Unable to delect latest tag for modeling app."
-      exit 1
-    fi
+writeVersionOnFile "${activiti_cloud_application_file}" "${version_dependency_aggregator}" "${name_dependency_aggregator}"
 
-    echo -n "activiti-modeling-app " >>$file
-    echo "$modeling_app_version" >>$file
+cd ../..
+updateRepoFile "${name_dependency_aggregator}" $activiti_cloud_file "${original_directory}"
+if [ "$name_dependency_aggregator" == "activiti-cloud-application" ]; then
+  updateRepoFile "${name_dependency_aggregator}" ${activiti_cloud_application_file} "${original_directory}"
+  updateRepoFile "${name_dependency_aggregator}" ${modeling_app_file} "${original_directory}"
+  updateRepoFile "${name_dependency_aggregator}" ${activiti_core_file} "${original_directory}"
+fi
 
-    #    write also on repos-activiti-cloud-modeling-app.txt that's used by docker push
-    echo -n "activiti-modeling-app " >>"$modeling_app_file"
-    echo "$modeling_app_version" >>"$modeling_app_file"
+if [ -n "$SHOULD_INCREMENT_VERSION" ]; then
+  updateRepoFile "${name_dependency_aggregator}" "VERSION" "${original_directory}"
+fi
 
-    activiti_core_version=$(mvn dependency:tree | grep "activiti-dependencies:pom:" -m1 | grep -o "7\.[[:digit:]]\{1,3\}\.[[:digit:]]\{1,5\}")
-
-    echo -n "Activiti " >>"${activiti_core_file}"
-    echo "${activiti_core_version}" >>"${activiti_core_file}"
-
-    echo -n "$name_dependency_aggregator " >>${bom_file}
-    echo "$version_dependency_aggregator" >>${bom_file}
-  else
-    echo -n "$name_dependency_aggregator " >>$file
-    echo "$version_dependency_aggregator" >>$file
-  fi
-
-  cd ../..
-  updateRepoFile "${project}" $file "${original_directory}"
-  if [ "$name_dependency_aggregator" == "activiti-cloud-application" ]; then
-    updateRepoFile "${project}" ${bom_file} "${original_directory}"
-    updateRepoFile "${project}" ${modeling_app_file} "${original_directory}"
-    updateRepoFile "${project}" ${activiti_core_file} "${original_directory}"
-  fi
-  if [ -n "$examples_file" ]; then
-    updateRepoFile "${project}" ${examples_file} "${original_directory}"
-  fi
-
-  if [ -n "$SHOULD_INCREMENT_VERSION" ]; then
-    updateRepoFile "${project}" "VERSION" "${original_directory}"
-  fi
-
-  rm -rf release-versions
-
-done
+rm -rf release-versions
---
+++ release-docker-activiti-cloud-example-images.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+set -e
+
+SCRIPT_DIR="$(cd "$( dirname "$0" )" && pwd)"
+
+export PROJECTS=activiti-cloud-application
+./clone-all.sh
+
+RELEASE_VERSION=$(<VERSION)
+export RELEASE_VERSION
+
+SRC_DIR=${SRC_DIR:-$HOME/src}
+echo "SCRIPT_DIR ${SRC_DIR}"
+cd ${SRC_DIR}/${PROJECTS} || exit 1
+
+git fetch --tags
+git checkout "${RELEASE_VERSION}"
+
+export DOCKER_IMAGES="example-runtime-bundle,activiti-cloud-query,example-cloud-connector,activiti-cloud-modeling"
+
+for DOCKER_IMAGE in ${DOCKER_IMAGES//,/ }
+do
+  cd ${DOCKER_IMAGE} || exit 1
+  ${SCRIPT_DIR}/dockerpush.sh
+  cd .. || exit 1
+done
+
