a1a0654d778d2a9d1bac08290cac87febf2d4995
Alex Merkulov <almerico@gmail.com>
2020-05-04 15:50:19 +0300

    Update release-helm.sh

--- release-helm.sh
+++ release-helm.sh
@@ -1,24 +1,16 @@
 #!/usr/bin/env bash
 set -ex
 
-output=$(echo "$VERSION" | grep -q "MOCK")
-if [ -z != "$output" ] ; then export HELM_ACTIVITI_VERSION=$(cat VERSION|rev|sed 's/\./-/'|sed 's/\./-/'|rev) ; fi
-echo HELM_ACTIVITI_VERSION=${HELM_ACTIVITI_VERSION}
-
 git clone https://${GITHUB_TOKEN}:x-oauth-basic@github.com/Activiti/activiti-cloud-application.git
 cp VERSION  activiti-cloud-application/activiti-cloud-dependencies/
 
 cd activiti-cloud-application/activiti-cloud-dependencies
 
-#mvn -q versions:set -Droot.log.level=off -DnewVersion=${VERSION}
 make updatebot/push-version-dry
 
 cat .updatebot-repos/github/activiti/activiti-cloud-full-chart/charts/activiti-cloud-full-example/requirements.yaml
 
 cd ..
-#TODO comment
-# make update-version-in-example-charts
-# make create-helm-charts-release-and-upload
 
 cd activiti-cloud-dependencies
 sleep 20
