f7b3190f857abbcb0190a4e75f26f6f9acb46ba1
Alex Merkulov <almerico@gmail.com>
2020-05-22 18:14:10 +0300

    release helm chart during travis release script (#33)
    * Update fetch-versions.sh based on activiti-cloud-application
    
    Fixes https://github.com/Activiti/Activiti/issues/3270
    
    * Simplify fetch-versions.sh
    
    * Update release job
    
    * Improve performance of fetch versions
    
    Use `grep` instead of `mvn dependency:tree`. This avoid resolving and downloading all the dependencies
    
    * [Release] 7.1.0.M7.MOCK8
    
    * Update versions - Activiti Cloud Dependencies
    
    * Set staging repository
    
    * Fix travis configuration
    
    * Fix release of examples images
    
    - stop using release-all only to switch of branch
    
    * Fix tag for modeling app docker image
    
    * Fix rename
    
    * Move to the right directory
    
    * kubernetes stuff
    
    * tag full chart
    
    * Update .travis.yml
    
    * chmod
    
    * Update .travis.yml
    
    * small fix
    
    * small fix
    
    * release variables
    
    * clean install missed
    
    * small fix
    
    * cp ..
    
    * - TRAVIS_WAIT_TIMEOUT=${TRAVIS_WAIT_TIMEOUT:-180}
    
    * moved rearrange
    
    * pwd
    
    * Version
    
    * functions added
    
    * source
    
    * travis wait removal
    
    * helm function
    
    * Update .travis.yml
    
    * updatebot
    
    * Update release-helm.sh
    
    * Update release-helm.sh
    
    * wrong repo
    
    * export PREVIEW_NAMESPACE=
    
    * Update release-helm.sh
    
    * update
    
    * spaces
    
    * rename version name
    
    * Update VERSION
    
    * release-helm.sh
    
    * helm version on mock
    
    * Update .travis.yml
    
    * Update .travis.yml
    
    * Update release-helm.sh
    
    * Update .travis.yml
    
    * Update release-helm.sh
    
    * Update .travis.yml
    
    * release-helm
    
    * .travis.yml
    
    * Update .travis.yml
    
    * Update .travis.yml
    
    * Update .travis.yml
    
    * Update release-helm.sh
    
    * Update release-helm.sh
    
    * Update .travis.yml
    
    * Update VERSION
    
    * Update release-helm.sh
    
    * Update release-helm.sh
    
    * Update release-helm.sh
    
    * Update .travis.yml
    
    * Update .travis.yml
    
    * Update release-helm.sh
    
    * Update VERSION
    
    * Update VERSION
    
    * Update release-helm.sh
    
    * Update release-helm.sh
    
    * Update .travis.yml
    
    * Update release-helm.sh
    
    * Delete release-helm-chart.sh
    
    * Update repos-activiti-cloud-application.txt
    
    * Update repos-activiti-cloud-application.txt
    
    * Update repos-activiti-cloud-application.txt
    
    * Update repos-activiti-cloud.txt
    
    * Update repos-activiti-modeling-app.txt
    
    * Update repos-activiti.txt
    
    * Update staging-repository.txt
    
    * Update repos-activiti-cloud-application.txt
    
    * Update .travis.java.yml
    
    caching
    
    * Update .travis.java.yml
    
    * Update release-helm.sh
    
    Co-authored-by: Elias Ricken de Medeiros <26007058+erdemedeiros@users.noreply.github.com>
    Co-authored-by: alfresco-build-user <build_user@alfresco.com>

---
+++ release-helm.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+set -ex
+
+git clone -b develop https://${GITHUB_TOKEN}:x-oauth-basic@github.com/Activiti/activiti-cloud-application.git
+cp VERSION  activiti-cloud-application/activiti-cloud-dependencies/
+
+cd activiti-cloud-application/activiti-cloud-dependencies
+
+make updatebot/push-version-dry
+
+cat .updatebot-repos/github/activiti/activiti-cloud-full-chart/charts/activiti-cloud-full-example/requirements.yaml
+
+cd ..
+
+make create-helm-charts-release-and-upload
+
+cd activiti-cloud-dependencies
+sleep 20
+make prepare-helm-chart
+make run-helm-chart
+sleep 300
+cd ../..
+
+cd activiti-cloud-application/activiti-cloud-acceptance-scenarios
+mvn -DskipITs -DskipTests -q clean install
+mvn -pl 'modeling-acceptance-tests' -Droot.log.level=off -q clean verify
+mvn -pl 'runtime-acceptance-tests'  -Droot.log.level=off -q clean verify
+cd -
+
+cd activiti-cloud-application/activiti-cloud-dependencies
+make replace-release-full-chart-names
+make prepare-helm-chart
+make tag
+make github
+cd -
+
