017b1147d24446e1240c138a1b95ac17833017b7
Elias Ricken de Medeiros <26007058+erdemedeiros@users.noreply.github.com>
2020-09-24 14:53:46 +0200

    Fix s3 options

--- prepare-release-ci.sh
+++ prepare-release-ci.sh
@@ -14,6 +14,6 @@ fi
 SCRIPT="${SCRIPT_DIR}/prepare-release.sh" "${SCRIPT_DIR}"/run.sh
 
 if [[ -n ${USE_CACHE} ]]; then
-  echo "Uploading cache to S3: aws s3 sync ${M2_REPOSITORY_DIR} ${S3_M2_REPOSITORY_RELEASE_DIR} ${S3_CLIENT_OPTS}"
-  time aws s3 sync ${M2_REPOSITORY_DIR} ${S3_M2_REPOSITORY_RELEASE_DIR} ${S3_CLIENT_OPTS}
+  echo "Uploading cache to S3: aws s3 sync ${M2_REPOSITORY_DIR} ${S3_M2_REPOSITORY_RELEASE_DIR}" --quiet --exclude "*" --include "*${RELEASE_VERSION}*"  --expires $(date -d '+10 days' --utc +'%Y-%m-%dT%H:%M:%SZ')
+  time aws s3 sync ${M2_REPOSITORY_DIR} ${S3_M2_REPOSITORY_RELEASE_DIR} --quiet --exclude "*" --include "*${RELEASE_VERSION}*" --expires $(date -d '+10 days' --utc +'%Y-%m-%dT%H:%M:%SZ')
 fi
--- shared-lib.sh
+++ shared-lib.sh
@@ -10,12 +10,11 @@ checkNoSnapshots() {
 
 initializeS3Variables(){
   S3_M2_REPOSITORY_RELEASE_DIR="${S3_M2_REPOSITORY_DIR}/${RELEASE_VERSION}"
-  S3_CLIENT_OPTS="--quiet --exclude * --include *activiti*${RELEASE_VERSION}* --expires $(date -d '+10 days' --utc +'%Y-%m-%dT%H:%M:%SZ')"
 }
 
 downloadFromS3(){
-  echo "Downloading cache from S3: aws s3 sync ${S3_M2_REPOSITORY_RELEASE_DIR} ${M2_REPOSITORY_DIR} ${S3_CLIENT_OPTS}"
-  time aws s3 sync ${S3_M2_REPOSITORY_RELEASE_DIR} ${M2_REPOSITORY_DIR} ${S3_CLIENT_OPTS}
+  echo "Downloading cache from S3: aws s3 sync ${S3_M2_REPOSITORY_RELEASE_DIR} ${M2_REPOSITORY_DIR} --quiet"
+  time aws s3 sync ${S3_M2_REPOSITORY_RELEASE_DIR} ${M2_REPOSITORY_DIR} --quiet
 }
 
 initializeReleaseVariables(){
