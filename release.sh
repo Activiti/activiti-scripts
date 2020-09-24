8dd01d06dc61b2729cf02c99a30ec3cc0d76ae79
Elias Ricken de Medeiros <26007058+erdemedeiros@users.noreply.github.com>
2020-09-24 17:53:11 +0200

    Remove prepare and build stages
    They doesn't seem to help in performance

--- release.sh
+++ release.sh
@@ -13,8 +13,6 @@ echo "RELEASING PROJECT $GIT_PROJECT from $(pwd)"
 echo "RELEASE_VERSION: $RELEASE_VERSION"
 echo "SCRIPT_DIR IS $SCRIPT_DIR"
 
-RELEASE_BRANCH=release-${RELEASE_VERSION}
-
 git fetch --tags
 if  git tag --list | egrep -q "^$RELEASE_VERSION$"
 then
@@ -44,10 +42,49 @@ else
   git config user.name "$GIT_AUTHOR_NAME"
   git config user.email "$GIT_AUTHOR_EMAIL"
 
-  git checkout "${RELEASE_BRANCH}"
+  for PROJECT in ${PROJECTS//,/ }
+  do
+    while read REPO_LINE;
+      do REPO_ARRAY=($REPO_LINE)
+      REPO=${REPO_ARRAY[0]}
+      if [ "${REPO}" = "${GIT_PROJECT}" ];
+      then
+        TAG=${REPO_ARRAY[1]}
+      fi
+    done < "$SCRIPT_DIR/repos-${PROJECT}.txt"
+  done
+
+  BASEBRANCH=${BASEBRANCH:-develop}
+
+  BASEBRANCHEXISTS=$(git ls-remote origin ${BASEBRANCH}) || true
+
+  if [ -n "$BASEBRANCHEXISTS" ];
+    then
+      echo 'using' $BASEBRANCH 'as base branch'
+    else
+      BASEBRANCH=develop
+  fi
+
+  if [ -z "${TAG}" ];
+  then
+    echo "Creating release branch from origin/${BASEBRANCH}..."
+    git checkout origin/${BASEBRANCH} -b release/${RELEASE_VERSION}
+  else
+    echo "Creating release branch from tags/v${TAG}..."
+    git checkout tags/v${TAG} -b release/${RELEASE_VERSION}
+  fi
+
+  VERSION=${SNAPSHOT_VERSION} NEXT_VERSION=${RELEASE_VERSION} . ${SCRIPT_DIR}/update-pom-version.sh
 
   if [ -n "${GIT_PUSH}" ]
   then
+    git add .
+    if [ -e "pom.xml" ];
+      then
+        git commit -m "updating to release version ${RELEASE_VERSION}"
+      else
+        git commit -m "updating to release version ${RELEASE_VERSION}" || true
+    fi
     git tag -a ${RELEASE_VERSION} -m "tagging release ${RELEASE_VERSION}"
     git checkout ${RELEASE_VERSION}
   fi
@@ -69,9 +106,9 @@ else
   then
     if [ -e "pom.xml" ];
     then
-      git push --atomic origin "${RELEASE_VERSION}" :"${RELEASE_BRANCH}"
+      git push --atomic origin ${RELEASE_VERSION}
     else
-      git push --atomic origin "${RELEASE_VERSION}" :"${RELEASE_BRANCH}"
+      git push --atomic origin ${RELEASE_VERSION} || true
     fi
   fi
 
