eb3e9abbd612a7bb2ed810b81832887404729172
Elias Ricken de Medeiros <26007058+erdemedeiros@users.noreply.github.com>
2020-11-27 16:37:42 +0100

    Update pattern to sync with S3
    Add prefix `org/activiti/`

--- release-ci.sh
+++ release-ci.sh
@@ -15,10 +15,10 @@ MAVEN_ARGS="clean install -DskipTests -q " ./release-all.sh
 
 if [[ -n ${USE_CACHE} ]]; then
   echo "Uploading cache to S3: aws s3 sync ${M2_REPOSITORY_DIR} ${S3_M2_REPOSITORY_RELEASE_DIR}" --quiet --exclude "*" \
-  --include "*${RELEASE_VERSION}*" --exclude "*javadoc.jar*" --exclude "*sources.jar*"\
+  --include "org/activiti/*${RELEASE_VERSION}*" --exclude "*javadoc.jar*" --exclude "*sources.jar*"\
   --expires $(date -d '+10 days' --utc +'%Y-%m-%dT%H:%M:%SZ')
 
   time aws s3 sync ${M2_REPOSITORY_DIR} ${S3_M2_REPOSITORY_RELEASE_DIR} --quiet --exclude "*" \
-  --include "*${RELEASE_VERSION}*" --exclude "*javadoc.jar*" --exclude "*sources.jar*"\
+  --include "org/activiti/*${RELEASE_VERSION}*" --exclude "*javadoc.jar*" --exclude "*sources.jar*"\
   --expires $(date -d '+10 days' --utc +'%Y-%m-%dT%H:%M:%SZ')
 fi
