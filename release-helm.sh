3257393989df8a7711d8089287903a6fadf22096
Elias Ricken de Medeiros <26007058+erdemedeiros@users.noreply.github.com>
2021-05-17 10:55:42 +0200

    set message broker and partition type

--- release-helm.sh
+++ release-helm.sh
@@ -23,7 +23,7 @@ make github
 cd - #return to activiti-cloud-application folder
 
 # override FRONT_RELEASE_VERSION otherwise it will use master tag for the docker image
-FRONTEND_VERSION=${VERSION} make install
+FRONTEND_VERSION=${VERSION} MESSAGING_BROKER=rabbitmq MESSAGING_PARTITIONED=partitioned make install
 
 attempt_counter=0
 max_attempts=50
