a91b9cdcac7dc220526e23f52165c72b20674274
Elias Ricken de Medeiros <26007058+erdemedeiros@users.noreply.github.com>
2022-04-07 17:39:28 +0200

    feat: add scripts to create maintenance branches
    - create branches and update configuration files

---
+++ create-maint-branch.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+set -e
+
+git checkout -b "$BRANCH"
+
+yq e -i '.github.organisations[].repositories[].branch = env(BRANCH)' .updatebot.yml
+
+yq e -i '.on.push.branches = [ env(BRANCH) ]' .github/workflows/main.yml
+yq e -i '.on.pull_request.branches = [ env(BRANCH) ]' .github/workflows/main.yml
+
+readonly NEXT_VERSION=${VERSION%\-SNAPSHOT}
+yq -i e '(.jobs.build.steps[] |  select(.id == "next-release") | .with.next-version) |=  env(NEXT_VERSION) ' .github/workflows/main.yml
+
+mvn -B versions:set -DnewVersion="$VERSION" -DprocessAllModules=true -DgenerateBackupPoms=false
+
+git add -u
+git commit -m "Configure branch $BRANCH"
+#git push origin "$BRANCH"
