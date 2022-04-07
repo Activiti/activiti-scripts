7dd66877db34268cc1a53b9c71972c2a7056f865
Elias Ricken de Medeiros <26007058+erdemedeiros@users.noreply.github.com>
2022-04-07 17:57:04 +0200

    Remove readonly modifier
    avoid problems when calling with more than one project. i.e. `PROJECTS=activiti,activiti-cloud,activiti-cloud-application ./create-maint-branch-ci.sh`

--- create-maint-branch.sh
+++ create-maint-branch.sh
@@ -8,7 +8,7 @@ yq e -i '.github.organisations[].repositories[].branch = env(BRANCH)' .updatebot
 yq e -i '.on.push.branches = [ env(BRANCH) ]' .github/workflows/main.yml
 yq e -i '.on.pull_request.branches = [ env(BRANCH) ]' .github/workflows/main.yml
 
-readonly NEXT_VERSION=${VERSION%\-SNAPSHOT}
+NEXT_VERSION=${VERSION%\-SNAPSHOT}
 yq -i e '(.jobs.build.steps[] |  select(.id == "next-release") | .with.next-version) |=  env(NEXT_VERSION) ' .github/workflows/main.yml
 
 mvn -B versions:set -DnewVersion="$VERSION" -DprocessAllModules=true -DgenerateBackupPoms=false
