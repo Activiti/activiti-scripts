6f635be56d9e28f4cd97615ec40d73f88aa6bb9d
Gian Carlo Pace <giancarlo.pace@hyland.com>
2022-04-13 12:15:37 +0200

    adding version and next version to the output variables

--- gh-actions-scripts/fetch-versions.sh
+++ gh-actions-scripts/fetch-versions.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+set -e
 
 writeVersionOnFile() {
   local destination_file=$1
@@ -49,34 +50,26 @@ updateRepoFile() {
 }
 
 original_directory=$(pwd)
-
-activiti_version_pattern="[0-9]*\.[0-9]*\.[0-9]*\-alpha\.[0-9]*"
+name_dependency_aggregator=activiti-cloud-application
 
 mkdir /tmp/release-versions && cd /tmp/release-versions || exit 1
-git clone -q https://github.com/Activiti/activiti-cloud-application.git && cd activiti-cloud-application || exit 1
+git clone -q https://github.com/Activiti/$name_dependency_aggregator.git && cd $name_dependency_aggregator || exit 1
 
 activiti_core_file=repos-activiti.txt
 activiti_cloud_file=repos-activiti-cloud.txt
 activiti_cloud_application_file=repos-activiti-cloud-application.txt
 modeling_app_file=repos-activiti-modeling-app.txt
 
-echo "Handling activiti-cloud-application"
+echo "Handling $name_dependency_aggregator"
 git fetch --tags
 
 tag_to_fetch="$1"
-tag_pattern=$activiti_version_pattern
+tag_pattern=${tag_pattern:-"*"}
 if [ -n "${tag_to_fetch}" ]; then
-  for k in $(git tag --list "$tag_pattern"); do
-    if [ "$k" = "${tag_to_fetch}" ]; then
-      exist=1
-      break
-    else
-      exist=0
-    fi
-  done
-
-  if [ "$exist" -eq 1 ]; then
-    git checkout -q tags/"${tag_to_fetch}"
+  set +e
+  git checkout -q tags/"${tag_to_fetch}"
+  set -e
+  if [ "$?" -eq "0" ]; then
     version_dependency_aggregator=${tag_to_fetch}
   else
     echo "The provided version '${tag_to_fetch}' does not exist"
@@ -95,15 +88,38 @@ fi
 
 parseActivitiCloudVersion $activiti_cloud_file
 
+if [ -n "$SHOULD_INCREMENT_VERSION" ]; then
+  ls
+  BETA_SUFFIX_PATTERN="\-beta[[:digit:]]\{1,3\}"
+  VERSION_PREFIX=$(curl https://raw.githubusercontent.com/Activiti/activiti-cloud-application/develop/pom.xml |
+    grep "<version>" | grep "\-SNAPSHOT" | grep -o -m1 "[[:digit:]]\{1,2\}\.[[:digit:]]\{1,3\}\.[[:digit:]]\{1,3\}")
+  LATEST_BETA_VERSION=$(git tag --sort=-creatordate | grep -m1 "${VERSION_PATTERN}${BETA_SUFFIX_PATTERN}")
+  if [ -z "$LATEST_BETA_VERSION" ]; then
+    NEXT_BETA_VERSION="${VERSION_PREFIX}-beta1"
+  else
+    echo "Latest beta version: ${LATEST_BETA_VERSION}"
+    CURRENT_COUNT=$(echo "${LATEST_BETA_VERSION}" | grep -o "${BETA_SUFFIX_PATTERN}" | grep -o "[[:digit:]]\{1,3\}")
+    echo "Current count: $CURRENT_COUNT"
+    NEXT_BETA_VERSION="${VERSION_PREFIX}-beta$((CURRENT_COUNT + 1))"
+  fi
+  echo "Next version: $NEXT_BETA_VERSION"
+  echo "${NEXT_BETA_VERSION}" >VERSION
+fi
+
 writeRepositoryWithoutVersionOnFile "$modeling_app_file" "activiti-modeling-app"
 
-writeVersionOnFile "${activiti_cloud_application_file}" "${version_dependency_aggregator}" "activiti-cloud-application"
+writeVersionOnFile "${activiti_cloud_application_file}" "${version_dependency_aggregator}" "${name_dependency_aggregator}"
 
 cd ../..
+updateRepoFile "${name_dependency_aggregator}" $activiti_cloud_file "${original_directory}"
+if [ "$name_dependency_aggregator" == "activiti-cloud-application" ]; then
+  updateRepoFile "${name_dependency_aggregator}" ${activiti_cloud_application_file} "${original_directory}"
+  updateRepoFile "${name_dependency_aggregator}" ${modeling_app_file} "${original_directory}"
+  updateRepoFile "${name_dependency_aggregator}" ${activiti_core_file} "${original_directory}"
+fi
 
-updateRepoFile "activiti-cloud-application" $activiti_cloud_file "${original_directory}"
-updateRepoFile "activiti-cloud-application" ${activiti_cloud_application_file} "${original_directory}"
-updateRepoFile "activiti-cloud-application" ${modeling_app_file} "${original_directory}"
-updateRepoFile "activiti-cloud-application" ${activiti_core_file} "${original_directory}"
+if [ -n "$SHOULD_INCREMENT_VERSION" ]; then
+  updateRepoFile "${name_dependency_aggregator}" "VERSION" "${original_directory}"
+fi
 
 rm -rf release-versions
