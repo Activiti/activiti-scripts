7fc8d3ea5feae4b000f0e5840f0325b88e10aeb9
Gian Carlo Pace <giancarlo.pace@hyland.com>
2022-04-14 23:17:30 +0200

    refactor: better naming wirh DOCKER_IMAGE
    Refs: gh-3903

--- gh-actions-scripts/tag-docker-image.sh
+++ gh-actions-scripts/tag-docker-image.sh
@@ -6,14 +6,14 @@ set -e
 BASE_TAG=${1:-BASE_TAG}
 RELEASE_TAG=${2:-RELEASE_TAG}
 DOCKERHUB_ORG=${3:-DOCKERHUB_ORG}
-DOCKERHUB_REPO=${4:-DOCKERHUB_REPO}
+DOCKER_IMAGE=${4:-DOCKER_IMAGE}
 DOCKERHUB_USERNAME=${5:-DOCKERHUB_USERNAME}
 DOCKERHUB_PASSWORD=${6:-DOCKERHUB_PASSWORD}
 
 # DOCKER VARS ################################
 AUTH_DOMAIN="auth.docker.io"
 AUTH_SERVICE="registry.docker.io"
-AUTH_SCOPE="repository:${DOCKERHUB_ORG}/${DOCKERHUB_REPO}:pull,push"
+AUTH_SCOPE="repository:${DOCKERHUB_ORG}/${DOCKER_IMAGE}:pull,push"
 AUTH_OFFLINE_TOKEN="1"
 AUTH_CLIENT_ID="shell"
 API_DOMAIN="registry-1.docker.io"
@@ -24,14 +24,14 @@ TOKEN_URL="https://${AUTH_DOMAIN}/token?service=${AUTH_SERVICE}&scope=${AUTH_SCO
 TOKEN=$(curl -X GET -u ${DOCKERHUB_USERNAME}:${DOCKERHUB_PASSWORD} $TOKEN_URL | jq -r '.token') && echo $TOKEN
 
 # Fetching the IMAGE MANIFEST ################
-MANIFESTS_URL="https://${API_DOMAIN}/v2/${DOCKERHUB_ORG}/${DOCKERHUB_REPO}/manifests/${BASE_TAG}"
+MANIFESTS_URL="https://${API_DOMAIN}/v2/${DOCKERHUB_ORG}/${DOCKER_IMAGE}/manifests/${BASE_TAG}"
 MANIFEST=$(curl -H "Accept: ${CONTENT_TYPE}" -H "Authorization: Bearer ${TOKEN}" "${MANIFESTS_URL}") && echo $MANIFEST
 
 curl -X PUT -H "Content-Type: ${CONTENT_TYPE}" \
          -H "Authorization: Bearer ${TOKEN}" \
          -d "${MANIFEST}" \
-         "https://${API_DOMAIN}/v2/${DOCKERHUB_ORG}/${DOCKERHUB_REPO}/manifests/${RELEASE_TAG}"
+         "https://${API_DOMAIN}/v2/${DOCKERHUB_ORG}/${DOCKER_IMAGE}/manifests/${RELEASE_TAG}"
 
-VERSIONS=$(curl -H "Authorization: Bearer ${TOKEN}" https://${API_DOMAIN}/v2/${DOCKERHUB_ORG}/${DOCKERHUB_REPO}/tags/list)
+VERSIONS=$(curl -H "Authorization: Bearer ${TOKEN}" https://${API_DOMAIN}/v2/${DOCKERHUB_ORG}/${DOCKER_IMAGE}/tags/list)
 
 echo $VERSIONS
